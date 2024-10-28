e7fc2e8c32a8d9bc3a86c9485d5e308cf028aedf
Hanan Fijananto <162029984+Yoloez@users.noreply.github.com>
2024-10-28 08:00:29 +0700

    Delete split & join.py

--- split & join.py
+++
@@ -1,9 +0,0 @@
-def split_and_join(line):
-    k = line.split()
-    s = "-".join(k)
-    return s
-
-if __name__ == '__main__':
-    line = input()
-    result = split_and_join(line)
-    print(result)
